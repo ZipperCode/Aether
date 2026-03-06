41c75d7656985e8b5ef294f9d3fc1f22b4a0a4ac
Zipper <906043695@qq.com>
2026-03-06 21:52:31 +0800

    feat: 数据库迁移

--- alembic/versions/20260306_1200_c2d5e0f7a9b1_merge_heads.py
+++ alembic/versions/20260306_1200_c2d5e0f7a9b1_merge_heads.py
@@ -3,7 +3,7 @@
 Merge multiple heads into a single head.
 
 Revision ID: c2d5e0f7a9b1
-Revises: 364680d1bc99, f0c3a7b9d1e2, 7b2a91d0c3ef
+Revises: 6a9b8c7d5e4f, 7b2a91d0c3ef
 Create Date: 2026-03-06 12:00:00.000000+00:00
 """
 
@@ -12,7 +12,7 @@ from __future__ import annotations
 from alembic import op
 
 revision = "c2d5e0f7a9b1"
-down_revision = ("364680d1bc99", "f0c3a7b9d1e2", "7b2a91d0c3ef")
+down_revision = ("6a9b8c7d5e4f", "7b2a91d0c3ef")
 branch_labels = None
 depends_on = None
 
