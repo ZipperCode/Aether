cfad8497174b535f1a279fbad6e4b7935c988d0f
Zipper <906043695@qq.com>
2026-03-06 21:14:01 +0800

    feat: update merge

---
+++ alembic/versions/20260306_1200_c2d5e0f7a9b1_merge_heads.py
@@ -0,0 +1,25 @@
+"""merge heads
+
+Merge multiple heads into a single head.
+
+Revision ID: c2d5e0f7a9b1
+Revises: 364680d1bc99, f0c3a7b9d1e2, 7b2a91d0c3ef
+Create Date: 2026-03-06 12:00:00.000000+00:00
+"""
+
+from __future__ import annotations
+
+from alembic import op
+
+revision = "c2d5e0f7a9b1"
+down_revision = ("364680d1bc99", "f0c3a7b9d1e2", "7b2a91d0c3ef")
+branch_labels = None
+depends_on = None
+
+
+def upgrade() -> None:
+    pass
+
+
+def downgrade() -> None:
+    pass
